28ea5efcc2e3698b41ad5aadddfdaf1b15e9574f
mh dev <matheushenrique22547@gmail.com>
2026-04-24 19:53:10 -0300

    JS Do site essenza

---
+++ data.js
@@ -0,0 +1,50 @@
+export const produtos = [
+    {
+        id: 0,
+        nome: 'Bolsa de Couro',
+        img: './assets/images/bolsa.jpeg',
+        tag: 'Acessórios',
+        descricao: 'Bolsa de couro legítimo com acabamento premium.',
+        preco: 349.90
+    },
+    {
+        id: 1,
+        nome: 'Tênis Branco',
+        img: './assets/images/tenis.jpeg',
+        tag: 'Calçados',
+        descricao: 'Tênis minimalista para o dia a dia.',
+        preco: 299.90
+    },
+    {
+        id: 2,
+        nome: 'Relógio Clássico',
+        img: './assets/images/relogio.jpeg',
+        tag: 'Acessórios',
+        descricao: 'Relógio elegante com pulseira de couro.',
+        preco: 599.90
+    },
+    {
+        id: 3,
+        nome: 'Óculos de Sol',
+        img: './assets/images/oculos.jpeg',
+        tag: 'Acessórios',
+        descricao: 'Óculos redondos com proteção UV.',
+        preco: 189.90
+    },
+    {
+        id: 4,
+        nome: 'Caneca Cerâmica',
+        img: './assets/images/caneca.jpeg',
+        tag: 'Casa',
+        descricao: 'Caneca artesanal em cerâmica branca.',
+        preco: 79.90
+    },
+    {
+        id: 5,
+        nome: 'Vela Aromática',
+        img: './assets/images/vela.jpeg',
+        tag: 'Casa',
+        descricao: 'Vela perfumada com fragrância amadeirada.',
+        preco: 119.90
+    }
+]
